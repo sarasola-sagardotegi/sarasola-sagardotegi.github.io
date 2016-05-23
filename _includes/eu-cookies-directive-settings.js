c7c7bd077a0fcf715124e5cf08ce1a7d274f59d8
Iñaki García Blasco <txorua@txorua.com>
2016-05-23 13:38:41 +0200

    Añado aviso legal y script para la EU Cookies Directive

---
+++ _includes/eu-cookies-directive-settings.js
@@ -0,0 +1,24 @@
+<script>
+$(document).ready(function(){
+  // Cookie setting script wrapper
+  var cookieScripts = function () {
+    // Internal javascript called
+    console.log("Running");
+		
+    // Loading external javascript file
+    $.cookiesDirective.loadScript({
+      uri:'external.js',
+	  appendTo: 'eantics'
+	});
+  }
+  $.cookiesDirective({
+    privacyPolicyUri: 'aviso-legal.html',
+    explicitConsent: false,
+    position : 'bottom',
+    //scriptWrapper: cookieScripts, 
+    cookieScripts: 'Google Analytics', 
+    backgroundColor: '#52B54A',
+    linkColor: '#ffffff'
+  });
+});
+</script>
